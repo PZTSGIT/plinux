a76befff5c330308a22b7742ceb547106c5b9b7b
PZTSGIT <605904200@qq.com>
2024-03-28 09:17:52 -0400

    update boot/head.s - push start function into stack and enter it by jmp and ret

--- boot/head.s
+++ boot/head.s
@@ -4,6 +4,18 @@
 _pg_dir:
 .globl startup_32
 startup_32:
-  call start 
-hold_on:
-  jmp hold_on
+  
+  jmp after_page_tables
+
+after_page_tables:
+  pushl $0
+  pushl $0
+  pushl $0
+  pushl $L6
+  pushl $start
+  jmp setup_paging
+L6:
+  jmp L6
+
+setup_paging:
+  ret
